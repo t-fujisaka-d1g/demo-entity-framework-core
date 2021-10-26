5aa5c769eabff62540a98848b11391a0151c46df
t-fujisaka <t-fujisaka@daiichi-g.co.jp>
2021-10-26 10:15:35 +0900

    dotnet ef migrations add AddReleasedAtColumnOfBooksTable

---
+++ Migrations/20211026011513_AddReleasedAtColumnOfBooksTable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace app.Migrations
+{
+    public partial class AddReleasedAtColumnOfBooksTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReleasedAt",
+                table: "Books",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReleasedAt",
+                table: "Books");
+        }
+    }
+}
