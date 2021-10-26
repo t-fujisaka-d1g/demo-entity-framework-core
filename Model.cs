ffb921dec7a209c2e70ae6feb227bd6ffabb0ad9
t-fujisaka <t-fujisaka@daiichi-g.co.jp>
2021-10-26 10:36:59 +0900

    モデルを構成するエンティティクラスを定義 - Qrクラス

--- Model.cs
+++ Model.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
 
 namespace app
@@ -6,6 +8,7 @@ namespace app
     class MyContext : DbContext
     {
         public DbSet<Book> Books => Set<Book>();
+        public DbSet<Qr> Qrs => Set<Qr>();
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var connectionString = "server=localhost;database=DemoEntityFrameworkCore;uid=SA;pwd=Pass1234;";
@@ -18,6 +21,30 @@ namespace app
         public int Id { get; set; }
         public string Name { get; set; } = "";
         public DateTime? ReleasedAt { get; set; }
+
+        // ナビゲーションプロパティ（Navigation Property）
+        private ICollection<Qr>? _qrs;
+        public ICollection<Qr> Qrs
+        {
+            set => _qrs = value;
+            get => _qrs ?? throw new InvalidOperationException("Uninitialized property: " + nameof(Qrs));
+        }
+    }
+
+    class Qr
+    {
+        [Key]
+        public string Code { get; set; } = "";
+        public int BookId { get; set; }
+        public string Url { get; set; } = "";
+
+        // ナビゲーションプロパティ（Navigation Property）
+        private Book? _book;
+        public Book Book
+        {
+            set => _book = value;
+            get => _book ?? throw new InvalidOperationException("Uninitialized property: " + nameof(Book));
+        }
     }
 
 }
