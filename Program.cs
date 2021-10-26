69fb3e6b92114457d5dbb27ba77a6a5e8f1558a8
t-fujisaka <t-fujisaka@daiichi-g.co.jp>
2021-10-26 09:50:43 +0900

    書籍テーブルにレコードを追加

--- Program.cs
+++ Program.cs
@@ -6,7 +6,29 @@ namespace app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var db = new MyContext())
+            {
+                try
+                {
+                    db.Database.BeginTransaction();
+
+                    // 書籍テーブルに追加(Insert)
+                    db.Books.Add(new Book { Name = "書籍A" });
+                    db.Books.Add(new Book { Name = "書籍B" });
+                    db.Books.Add(new Book { Name = "書籍C" });
+                    db.SaveChanges();
+
+                    db.Database.CommitTransaction();
+                    Console.WriteLine($"DB更新成功");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"DB更新失敗");
+                    Console.WriteLine($"(ex: {ex.Message})");
+                    db.Database.RollbackTransaction();
+                }
+
+            }
         }
     }
 }
