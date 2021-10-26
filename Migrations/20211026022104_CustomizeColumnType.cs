b971e0c2504ecfc998492818bf733a1bed2c1b32
t-fujisaka <t-fujisaka@daiichi-g.co.jp>
2021-10-26 11:21:11 +0900

    dotnet ef migrations add CustomizeColumnType

---
+++ Migrations/20211026022104_CustomizeColumnType.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace app.Migrations
+{
+    public partial class CustomizeColumnType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "X001BOOKNAME",
+                table: "X001BOOKS",
+                type: "nvarchar(128)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "X001BOOKNAME",
+                table: "X001BOOKS",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)");
+        }
+    }
+}
